139484968c5d05565488d8d375d7044e8ba29394
manjula-0612 <manjular0612@gmail.com>
2025-08-06 21:21:58 +0530

    Add files via upload
    day1

---
+++ day1/Chrome.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float version=91.0f;
+		String developer="Google";
+		Boolean isBeta=false;
+		int releaseYear=2008;
+		char shortcutKey='C';
+		
+         System.out.println("The version number is"+" "+version);
+         System.out.println("The developer is"+" "+developer);
+         System.out.println("The isBeta is"+" "+isBeta);
+         System.out.println("The releaseYear is"+" "+releaseYear);
+         System.out.println("The shortcutKey is"+" "+shortcutKey);
+	}
+
+}
